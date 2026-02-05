efab472b031fc64dea17692587ebc7e6a7aba8d3
NattananKat <natkat.nattanan@gmail.com>
2026-02-05 20:01:23 +0700

    ณัฐนันท์

--- Project.cpp
+++ Project.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
+#include <windows.h>
 
 using namespace std;
 
 int main(){
-    
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    string name;
+
+    cout << "Chatbot : สวัสดีครับ! ยินดีต้อนรับสู่ ENGR CMU Chat Bot" << endl ;
+    cout << "Chatbot : ก่อนอื่น บอกชื่อของคุณให้ผมรู้จักหน่อยได้ไหมครับ?" << endl ;
+    cout << "User : ";
+    getline(cin,name);
+
+    cout << "Chatbot : " << "ยินดีที่ได้รู้จักครับ คุณ "<< name <<"! เพื่อความแม่นยำของข้อมูล" << "\n" << "กรุณาเลือกสถานะของคุณหน่อยนะครับ:" << endl;
+
+
 }
